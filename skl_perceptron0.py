7af5dc6192ecefea62b444b72a1efb6b24ebb2fb
kljp <slashxp@naver.com>
2017-11-09 17:29:41 +0900

    skicit-learn practice added

---
+++ skl_perceptron0.py
@@ -0,0 +1,26 @@
+from sklearn import datasets
+from sklearn.cross_validation import  train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+if __name__ == '__main__':
+
+    iris = datasets.load_iris()
+    X = iris.data[:, [2, 3]]
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+    sc = StandardScaler()
+    sc.fit(X_train)
+    X_train_std = sc.transform(X_train)
+    X_test_std = sc.transform(X_test)
+
+    ml = Perceptron(eta0=0.01, n_iter=40, random_state=0)
+    ml.fit(X_train_std, y_train)
+    y_pred = ml.predict(X_test_std)
+
+    print('총 테스트 개수: %d, 오류개수:%d' % (len(y_test), (y_test != y_pred).sum()))
+    print('정확도: %2f' % accuracy_score(y_test, y_pred))
